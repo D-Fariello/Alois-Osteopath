675b831b0966157ff27bf1cfb3abba888194aa87
Dalila <dalila.fariello@gmail.com>
2025-01-20 20:28:53 +0100

    feat: start implementing Navbar header component

--- osteopath-project/src/components/Header.jsx
+++ osteopath-project/src/components/Header.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="header">
+      <div className="logo-Image">
+        <NavLink to="/" className="main-nav-logo">
+          <img
+            src="./images/LogoColored.png"
+            alt="Logo web"
+            className="main-nav-logo-image"
+          />
+        </NavLink>
+      </div>
+      <nav>
+        <ul className="nav-bar">
+          <li>
+            <a href="#acceuil">Acceuil</a>
+          </li>
+          <li>
+            <a href="#a-propos">A propos</a>
+          </li>
+          <li>
+            <a href="#tarif">Tarif</a>
+          </li>
+          <li>
+            <a href="#avis">Avis</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#specialites">Specialités</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
